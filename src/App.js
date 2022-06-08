9e40c8d7934b339e73df180ff6790019400483d3
Eliel <elielkaragua2016@gamil.com>
2022-06-08 16:25:53 -0300

    eliel

--- src/App.js
+++ src/App.js
@@ -1,41 +1,73 @@
 import './App.css';
 import {Container, Form, Button, Row, Col} from 'react-bootstrap'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 
 
-const[titulo,setTitulo] = useState("");
-const[prazo,setPrazo] = useState("");
 
 function App() {
+  const[titulo,setTitulo] = useState("");
+  const[prazo,setPrazo] = useState("");
+  const [imagem, setImagem] = useState(null);
+  const [urlImagem, setUrlImagem] = useState("");
+
+
+  useEffect(() => {
+    if (!imagem) return;
+    const url = URL.createObjectURL(imagem);
+    setUrlImagem(url);
+  }, [imagem]);
 
   function tratarSubmit(event){
     event.preventDefault();
-    console.log({titulo,prazo});
-  }
+    console.log({titulo,prazo,imagem});
+  } 
 
   return (
     <Container className='text'>
-      <h1>
-        Lista Financeira        
-      </h1>
+      <h1>Lista Financeira</h1>
       <Form onSubmit={tratarSubmit}>
         <Row className='gy-2'>
           <Col xs={12} md={6} lg={9}>
-            <Form.Group controlId='titulo'>
+            <Form.Group as={Row} controlId='titulo'>
+            <Col xs={12} lg={2}>
               <Form.Label>Título</Form.Label>
-              < Form.Control type='text'/>
+            </Col>
+            <Col xs={12} lg={10}>
+              <Form.Control 
+              type='text'
+              onChange={(event) => setTitulo(event.target.value)}
+              value={titulo}/>
+            </Col>
             </Form.Group>          
           </Col>
-          <Col xs={12} md={6} lg={3}>
-            <Form.Group controlId='prazo'>
+          <Col xs={12} md={6} lg={4}>
+            <Form.Group  as={Row} controlId='prazo'>
+            <Col xs={12} lg={3}>
               <Form.Label>Prazo</Form.Label>
-              < Form.Control type='date'/>
+            </Col>
+            <Col xs={12} lg={9}>
+              <Form.Control 
+                type='date'
+                onChange={(event) => setPrazo(event.target.value)}
+                value={prazo}
+                />
+              </Col>
             </Form.Group>          
           </Col>
           <Col xs={12}>
-            <Form.Group controlId='imagem'>
-              <Form.Label>Imagem de Capa</Form.Label>
-              < Form.Control type='file'/>
+            <Form.Group as={Row} controlId='imagem'>
+              <Col xs={12}>
+                <Form.Label>Imagem de Capa</Form.Label>
+              </Col>
+              <Col xs={12}>
+                <Form.Control
+                  type='file'
+                  onChange={(event) => setImagem(event.target.files[0])} 
+                />
+              </Col>
+              <Col xs={12}>
+                <img src={urlImagem} />
+              </Col>
             </Form.Group>          
           </Col> 
           <Col xs={12} className='text-center'>
@@ -47,4 +79,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
